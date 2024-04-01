4361a23a963dae60ffe37be07f5970af7af048c3
29usama <muhammadusama2117@gmail.com>
2024-04-01 22:41:22 +0500

    abc

---
+++ main.tf
@@ -0,0 +1,26 @@
+provider "google" {
+  project = var.project_id
+  region  = var.region
+}
+
+# Create the shared VPC network in the host project
+resource "google_compute_shared_vpc_host_project" "host_project" {
+  project = var.project_id
+}
+
+# Define the shared VPC network
+resource "google_compute_shared_vpc" "shared_vpc" {
+  provider      = google
+  host_project  = google_compute_shared_vpc_host_project.host_project.project
+  name          = var.shared_network_name
+}
+
+# Create a subnet in the shared VPC network
+resource "google_compute_subnetwork" "subnet" {
+  name              = var.subnet_name
+  ip_cidr_range     = var.subnet_cidr
+  region            = var.region
+  network           = google_compute_shared_vpc.shared_vpc.self_link
+}
+
+# Define other resources as needed (firewall rules, etc..)
---
+++ variables.tf
@@ -0,0 +1,31 @@
+variable "project_id" {
+  description = "The ID of the GCP project"
+  default = "gmzdev"
+}
+
+variable "region" {
+  description = "The region in which the resources will be created"
+  default     = "us-central1"
+}
+
+variable "shared_vpc_name" {
+  description = "The name of the shared VPC host project"
+  default     = "shared_vpc_ilmiya_1"
+}
+
+variable "shared_network_name" {
+  description = "The name of the shared VPC network"
+  default = "shared_network_ilmiya_1"
+}
+
+variable "subnet_name" {
+  description = "The name of the subnet"
+  default = "shared_subnet_ilmiya_1"
+}
+
+variable "subnet_cidr" {
+  description = "CIDR block for the subnet"
+  default     = "10.10.0.0/24"
+}
+
+# Add other variables as needed
