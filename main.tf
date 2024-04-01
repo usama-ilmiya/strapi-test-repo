ff434c7b63ca39d691a2d049fcf37f14b61fec61
29usama <muhammadusama2117@gmail.com>
2024-04-01 23:40:07 +0500

    lllll

--- main.tf
+++ main.tf
@@ -1,26 +1,26 @@
-provider "google" {
+provider "google-beta" {
   project = "env-ilmiya-dev"  # Replace with your actual project ID
   region  = var.region
 }
 
 # Create the shared VPC network in the host project
-resource "google_compute_shared_vpc_host_project" "host_project" {
+resource "google-beta_compute_shared_vpc_host_project" "host_project" {
   project = "env-ilmiya-net"  # Replace with your actual host project name
 }
 
 # Define the shared VPC network
-resource "google_compute_shared_vpc" "shared_vpc" {
-  provider      = google
-  host_project  = google_compute_shared_vpc_host_project.host_project.id
+resource "google-beta_compute_shared_vpc" "shared_vpc" {
+  provider      = google-beta
+  host_project  = google-beta_compute_shared_vpc_host_project.host_project.id
   name          = var.shared_network_name
 }
 
 # Create a subnet in the shared VPC network
-resource "google_compute_subnetwork" "subnet" {
+resource "google-beta_compute_subnetwork" "subnet" {
   name              = var.subnet_name
   ip_cidr_range     = var.subnet_cidr
   region            = var.region
-  network           = google_compute_shared_vpc.shared_vpc.id
+  network           = google-beta_compute_shared_vpc.shared_vpc.id
 }
 
 # Define other resources as needed (firewall rules, etc..)
